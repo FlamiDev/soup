743f831f73f72f7b0f472191af5bdb962352de2a
Joel Vermeulen <joel.vermeulen5@outlook.com>
2025-06-07 23:53:43 +0200

    Start new parser architecture and transition from type-based parser_lib to function-based cauldron

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,18 @@ mod errors;
 mod parser;
 
 fn main() {
+    let mut args: VecDeque<String> = std::env::args().collect();
+    args.pop_front();
+    let Some(file) = args.pop_front() else {
+        print!("No input file given");
+        return;
+    };
+    let input = std::fs::read_to_string(file).expect("Failed to read file");
+    let lexed = cauldron::lex(input.as_str());
+    println!("{:?}", lexed);
+}
+
+fn _main() {
     setup_logging();
 
     let mut args: VecDeque<String> = std::env::args().collect();
